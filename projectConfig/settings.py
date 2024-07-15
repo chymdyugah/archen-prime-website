641cc370f0360ebc1c5b48a74032369797b0e989
chymdy <ugahchymdy@gmail.com>
2024-07-15 01:45:56 +0100

    fix: static

--- projectConfig/settings.py
+++ projectConfig/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static-root'
 STATICFILES_DIRS = [BASE_DIR/'static-root',]
 MEDIA_ROOT = 'media'
 MEDIA_URL = 'media/'
